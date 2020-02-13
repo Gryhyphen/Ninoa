14e61565fa9bbbe3c23bf4a316e836bf6bdd1b00
Gry- <gryhyphen@ninoa.lan>
2020-02-13 14:23:17 +1000

    Replace next() with continue() in head.
    Continue operates via an entirely different principal then next.

--- src/ninoa/head/Head.js
+++ src/ninoa/head/Head.js
@@ -4,23 +4,53 @@ export default class Head {
         this.engine = engineWrapper;
     }
 
-    init() {
-        this.position.onEnter(this);
+    continue() {
+        if (this.position === null) throw new Error("NO MORE NODES!");
+
+        const localStack = [{ position: this.position, shouldLeave: false }];
+
+        while (localStack.length > 0) {
+            // Grab values from stack
+            let { position: currentPos, shouldLeave: leave } = localStack.pop();
+
+            // Check if reached end of nodes or attempting to enter nothing.
+            if (currentPos === null) {
+                // If reached the last node and not starting
+                this.position = null;
+                break;
+            }
+
+            // Calculating isThereSiblingsLeftToProcess
+            // Also null checking on currentPos.parent in case this is the root.
+            const parentLength = (currentPos.parent) ? currentPos.parent.children.length : null;
+            const currentIndex = (currentPos.parent) ? currentPos.parent.children.findIndex(child => child === currentPos) : null;
+
+            const isThereSiblingsLeftToProcess = (currentPos.parent) ? currentIndex + 1 < parentLength : false;
+
+            if (isThereSiblingsLeftToProcess) {
+                localStack.push({ position: currentPos.parent.children[currentIndex + 1], shouldLeave: false });
+            } else {
+                localStack.push({ position: currentPos.parent, shouldLeave: true });
+            }
+
+            // Performing lifecycle methods
+            if (leave) {
+                currentPos.onLeave(this);
+            } else if (currentPos.children) { // Check if you need to enter the children
+                currentPos.onEnter(this);
+                localStack.push({ position: currentPos.children[0], shouldLeave: false });
+            } else { // otherwise iterate through children
+                currentPos.onEnter(this);
+                currentPos.onLeave(this); // current node must be a leaf node for this to be true.
+            }
+
+            // Break if should not process next item on stack based on current auto continue rules.
+            const shouldAutoContinue = currentPos.shouldAutoContinue() || leave;
+            if (!shouldAutoContinue) {
+                this.position = localStack.pop().position;
+                break;
+            }
+        }
     }
 
-    next() {
-        this.position.onLeave(this);
-        this.position = this.position.next(this);
-        this.position.onEnter(this);
-    }
-
-    continue() { // Continue might be a better semantic? Like a debugger.
-        this.next();
-        // this.position.onHalt(); // Need to add an onHalt() lifecycle method to elements
-        // Maybe not, didn't seem to help
-    }
-
-    back() {
-        this.position = this.position.back(this);
-    }
 }
